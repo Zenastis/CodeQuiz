dd3f41d283d46d5cc5351580c40ef129b1fc4d20
samuel mcswain <samuelmcswain@hotmail.com>
2021-02-15 15:45:41 -0500

    CodeQuiz functions created

---
+++ script.js
@@ -0,0 +1,51 @@
+var timeEl = document.querySelector(".timer")
+
+
+function questionOne(){
+    document.textContent("Commonly used data types DO NOT include?")
+    
+    
+    var answersQ1 =[
+        "Strings",
+        "Numbers",
+        "Boolean",
+        "Alerts"
+    ]
+
+}
+
+function questionTwo(){
+    var answersQ2 =[
+        "Numbers & Strings",
+        "Other Arrays",
+        "Booleans",
+        "All The Above"
+    ]
+}
+
+function questionThree(){
+    var answersQ3 =[
+        "Quotes",
+        "Curly Braces",
+        "Parenthesis",
+        "Square Brackets"
+    ]
+}
+
+function questionFour(){
+    var answersQ4 =[
+        "Commas",
+        "Curly Brackets",
+        "Quotes",
+        "Parenthesis"
+    ]
+}
+
+function questionFive(){
+    var answersQ5 =[
+        "JavaScript",
+        "Terminal/Bash",
+        "ForLoops",
+        "Console.log"
+    ]
+}
